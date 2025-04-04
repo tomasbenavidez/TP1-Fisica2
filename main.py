8befe75fd7b4b3c77f1b7d85d061fe9abb7b41d3
Anaholcman <anitaholcman@gmail.com>
2025-04-04 15:29:29 -0300

    Con errores

--- main.py
+++ main.py
@@ -8,35 +8,22 @@ sns.set_theme(style="whitegrid")
 # Cargar los datos
 data = pd.read_csv('datos.csv', header=None)
 
-# Separar las secciones de datos
+#---------Resistencia 10----------
+
 resistencia_10k = data.iloc[:26, :2]  # Filas correspondientes a la resistencia de 10k ohm
 resistencia_10k.columns = ['Voltaje (V)', 'Corriente (A)']
-
-resistencia_96_8 = data.iloc[28:42, :2]  # Filas correspondientes a la resistencia de 96.8 ohm en kohm 
-resistencia_96_8.columns = ['Voltaje (V)', 'Corriente (A)']
-
-resistencia_4_62k = data.iloc[43:54, :2]  # Filas correspondientes a la resistencia de 4.62k ohm
-resistencia_4_62k.columns = ['Voltaje (V)', 'Corriente (A)']
-resistencia_4_62k = resistencia_4_62k.apply(pd.to_numeric, errors='coerce').dropna()
-
-# Convertir a valores numéricos
 resistencia_10k = resistencia_10k.apply(pd.to_numeric, errors='coerce').dropna()
-resistencia_96_8 = resistencia_96_8.apply(pd.to_numeric, errors='coerce').dropna()
-
-# Recta lineal teórica (usando ley de Ohm)
-# I = V / R
 resistencia_10k['Corriente Teórica (A)'] = resistencia_10k['Voltaje (V)'] / 9.94
-resistencia_96_8['Corriente Teórica (A)'] = resistencia_96_8['Voltaje (V)'] / 0.0968
-
-# Residuos
 resistencia_10k['Residuos'] = resistencia_10k['Corriente (A)'] - resistencia_10k['Corriente Teórica (A)']
-resistencia_96_8['Residuos'] = resistencia_96_8['Corriente (A)'] - resistencia_96_8['Corriente Teórica (A)']
+
+resistencia_10k['Error abs'] = resistencia_10k['Residuos'].abs()
 
 
-# Graficar los datos para resistencia de 10kΩ
+
+# Grafico
 plt.figure(figsize=(10, 6))
 plt.plot(resistencia_10k['Voltaje (V)'], resistencia_10k['Corriente (A)'], label='Experimental', marker='o', color='darkblue')
-plt.plot(resistencia_10k['Voltaje (V)'], resistencia_10k['Corriente Teórica (A)'], label='Teórica', color='orange')
+plt.plot(resistencia_10k['Voltaje (V)'].tolist(), resistencia_10k['Corriente Teórica (A)'].tolist(), label='Teórica', color='orange')
 
 # Conectar los puntos experimentales con la recta teórica
 for i in range(len(resistencia_10k)):
@@ -46,18 +33,38 @@ for i in range(len(resistencia_10k)):
         color='gray', linestyle=':', linewidth=1
     )
 
-# Configuración del gráfico 1
 #plt.title('Voltaje vs Corriente para Resistencia de 10kΩ', fontsize=16, fontweight='bold')
+plt.errorbar(
+    resistencia_10k['Voltaje (V)'],
+    resistencia_10k['Corriente (A)'],
+    yerr=resistencia_10k['Error abs'],
+    fmt='o',
+    label='Error abs',
+    color='darkblue',
+    ecolor='red',
+    capsize=4
+)
 plt.xlabel('Voltaje (V)', fontsize=14)
 plt.ylabel('Corriente (A)', fontsize=14)
 plt.legend(fontsize=12)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
+plt.xlim(0, 25)
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
 
-# Graficar los datos para resistencia de 96.8Ω
+#---------Resistencia 96.0----------
+
+
+resistencia_96_8 = data.iloc[28:42, :2]  # Filas correspondientes a la resistencia de 96.8 ohm en kohm 
+resistencia_96_8.columns = ['Voltaje (V)', 'Corriente (A)']
+resistencia_96_8 = resistencia_96_8.apply(pd.to_numeric, errors='coerce').dropna()
+resistencia_96_8['Corriente Teórica (A)'] = resistencia_96_8['Voltaje (V)'] / 0.0968
+resistencia_96_8['Residuos'] = resistencia_96_8['Corriente (A)'] - resistencia_96_8['Corriente Teórica (A)']
+resistencia_96_8['Error abs'] = resistencia_96_8['Residuos'].abs()
+
+
 plt.figure(figsize=(10, 6))
 plt.plot(resistencia_96_8['Voltaje (V)'], resistencia_96_8['Corriente (A)'], label='Experimental', marker='o', color='darkblue')
 plt.plot(resistencia_96_8['Voltaje (V)'], resistencia_96_8['Corriente Teórica (A)'], label='Teórica', color='orange')
@@ -69,8 +76,18 @@ for i in range(len(resistencia_96_8)):
         color='gray', linestyle=':', linewidth=1
     )
 
-# Configuración del gráfico 2
+#gráfico 2
 #plt.title('Voltaje vs Corriente para Resistencia de 96.8Ω', fontsize=16, fontweight='bold')
+plt.errorbar(
+    resistencia_96_8['Voltaje (V)'],
+    resistencia_96_8['Corriente (A)'],
+    yerr=resistencia_96_8['Error abs'],
+    fmt='o',
+    label='Error abs',
+    color='darkblue',
+    ecolor='red',
+    capsize=4
+)
 plt.xlabel('Voltaje (V)', fontsize=14)
 plt.ylabel('Corriente (A)', fontsize=14)
 plt.legend(fontsize=12)
@@ -81,9 +98,18 @@ plt.tight_layout()
 plt.show()
 
 
+
+# ---------Resistencia 4.62k----------
+
+resistencia_4_62k = data.iloc[43:54, :2]  # Filas correspondientes a la resistencia de 4.62k ohm
+resistencia_4_62k.columns = ['Voltaje (V)', 'Corriente (A)']
+resistencia_4_62k = resistencia_4_62k.apply(pd.to_numeric, errors='coerce').dropna()
+
 R_teorico = 4.62  # en kΩ
 resistencia_4_62k['Corriente Teórica (A)'] = resistencia_4_62k['Voltaje (V)'] / R_teorico
 resistencia_4_62k['Residuos'] = resistencia_4_62k['Corriente (A)'] - resistencia_4_62k['Corriente Teórica (A)']
+resistencia_4_62k['Error abs'] = resistencia_4_62k['Residuos'].abs()
+
 
 # Graficar
 plt.figure(figsize=(10, 6))
@@ -99,8 +125,18 @@ for i in range(len(resistencia_4_62k)):
         [resistencia_4_62k['Corriente (A)'].iloc[i], resistencia_4_62k['Corriente Teórica (A)'].iloc[i]],
         color='gray', linestyle=':', linewidth=1
     )
-# Configuracion del gráfico 3
+# gráfico 3
 #plt.title('Voltaje vs Corriente para Resistencia de 4.62kΩ', fontsize=16, fontweight='bold')
+plt.errorbar(
+    resistencia_4_62k['Voltaje (V)'],
+    resistencia_4_62k['Corriente (A)'],
+    yerr=resistencia_4_62k['Error abs'],
+    fmt='o',
+    label='Error abs',
+    color='darkblue',
+    ecolor='red',
+    capsize=4
+)
 plt.xlabel('Voltaje (V)', fontsize=14)
 plt.ylabel('Corriente (A)', fontsize=14)
 plt.legend(fontsize=12)
